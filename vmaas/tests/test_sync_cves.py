0d7a1e73a37dd494159af72d9f9af97eec25d6f7
Martin Kourim <mkourim@redhat.com>
2018-04-03 16:10:12 +0200

    Wait until sync can be started

--- vmaas/tests/test_sync_cves.py
+++ vmaas/tests/test_sync_cves.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import pytest
+from wait_for import wait_for
+
+from utils.rest import exceptions
 
 
-@pytest.mark.skip(reason='GH#166')
 def test_sync_cves(rest_api):
-    response = rest_api.cvescan().response_check()
+    def _refresh():
+        try:
+            response = rest_api.cvescan()
+        except exceptions.ClientError as err:
+            if 'Another sync task already in progress' in err.response.body['msg']:
+                return False
+            raise
+        return response
+
+    response, __ = wait_for(_refresh, num_sec=10)
+    response.response_check()
     response, = response
     assert 'CVE sync task started' in response.msg
--- vmaas/tests/test_sync_repos.py
+++ vmaas/tests/test_sync_repos.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import pytest
+from wait_for import wait_for
+
+from utils.rest import exceptions
 
 
-@pytest.mark.skip(reason='GH#166')
 def test_repo_refresh(rest_api):
-    response = rest_api.reporefresh().response_check()
+    def _refresh():
+        try:
+            response = rest_api.reporefresh()
+        except exceptions.ClientError as err:
+            if 'Another sync task already in progress' in err.response.body['msg']:
+                return False
+            raise
+        return response
+
+    response, __ = wait_for(_refresh, num_sec=10)
+    response.response_check()
     response, = response
     assert 'Repo sync task started' in response.msg
