134d377ff77b18e119a10a719db8d0fdb674b9a8
Martin Kourim <mkourim@redhat.com>
2018-04-03 16:11:32 +0200

    Enhance updates tests

--- utils/rest/tools.py
+++ utils/rest/tools.py
@@ -5,6 +5,12 @@ REST API helper functions
 
 import datetime
 
+EXPECTED_UPDATE_KEYS = [
+    'erratum',
+    'repository',
+    'package',
+]
+
 
 def cves_body(cves):
     return dict(cve_list=cves)
@@ -27,3 +33,66 @@ def updates_body(packages, repositories=None, modified_since=None):
             modified_since = modified_since.replace(microsecond=0).isoformat()
         body['modified_since'] = modified_since
     return body
+
+
+def check_updates_uniq(updates):
+    known_records = []
+    not_unique = []
+    for update in updates:
+        for record in known_records:
+            if record != update:
+                continue
+            # If are here, the record is already known.
+            # Making sure the record is added to `not_unique` list just once.
+            for seen in not_unique:
+                if seen == update:
+                    break
+            else:
+                not_unique.append(record)
+            break
+        else:
+            known_records.append(update)
+
+    assert not not_unique, 'Duplicates found: {!r}'.format(not_unique)
+
+
+def _compare_updates(expected_update, available_update):
+    for key, value in expected_update.items():
+        if key not in available_update:
+            break
+        if key == 'package' and value in available_update[key]:
+            continue
+        if value == available_update[key]:
+            continue
+    else:
+        return True
+    return False
+
+
+def check_expected_updates(expected_updates, available_updates):
+    not_found = []
+    for expected_update in expected_updates:
+        for available_update in available_updates:
+            if _compare_updates(expected_update, available_update):
+                break
+        else:
+            not_found.append(expected_update)
+    assert not not_found, 'Expected update not found: {!r}'.format(not_found)
+
+
+def validate_package_updates(package, expected_updates):
+    assert package.description
+    assert package.summary
+
+    # check that expected keys are available in each record
+    for record in package.available_updates:
+        for key in EXPECTED_UPDATE_KEYS:
+            assert record[key] is not None, 'Expected key `{}` has no value'.format(key)
+
+    # check that expected updates are present in the response
+    assert len(package.available_updates) >= len(expected_updates)
+    check_expected_updates(expected_updates, package.available_updates)
+
+    # check that available updates records are unique
+    # disabled until GH#176 is fixed
+    # check_updates_uniq(package.available_updates)
--- vmaas/tests/test_updates.py
+++ vmaas/tests/test_updates.py
@@ -4,18 +4,142 @@ import pytest
 
 from utils.rest import tools
 
+
+EXPECTED_BASH = [
+    {
+        "erratum": "RHSA-2017:1931",
+        "repository": "rhel-7-server-rpms",
+        "package": "bash-"
+    },
+    {
+        "erratum": "RHSA-2017:1931",
+        "repository": "rhel-7-workstation-rpms",
+        "package": "bash-"
+    },
+]
+
+EXPECTED_BASH_W_REPO = [
+    {
+        "erratum": "RHSA-2017:1931",
+        "repository": "rhel-7-server-rpms",
+        "package": "bash-"
+    },
+]
+
+EXPECTED_POSTGRES = [
+    {
+        "erratum": "RHSA-2017:3402",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-"
+    },
+    {
+        "erratum": "RHSA-2017:3402",
+        "repository": "rhel-7-workstation-rpms",
+        "package": "postgresql-"
+    },
+    {
+        "erratum": "RHSA-2017:2728",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-"
+    },
+    {
+        "erratum": "RHSA-2017:2728",
+        "repository": "rhel-7-workstation-rpms",
+        "package": "postgresql-"
+    },
+    {
+        "erratum": "RHSA-2017:1983",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-"
+    },
+    {
+        "erratum": "RHSA-2017:1983",
+        "repository": "rhel-7-workstation-rpms",
+        "package": "postgresql-"
+    },
+]
+
+EXPECTED_POSTGRES_W_REPO = [
+    {
+        "erratum": "RHSA-2017:3402",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-"
+    },
+    {
+        "erratum": "RHSA-2017:2728",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-"
+    },
+    {
+        "erratum": "RHSA-2017:1983",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-"
+    },
+]
+
+EXPECTED_POSTGRES_DEVEL = [
+    {
+        "erratum": "RHSA-2017:1983",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-devel-"
+    },
+    {
+        "erratum": "RHSA-2017:1983",
+        "repository": "rhel-7-workstation-rpms",
+        "package": "postgresql-devel-"
+    },
+    {
+        "erratum": "RHSA-2017:2728",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-devel-"
+    },
+    {
+        "erratum": "RHSA-2017:2728",
+        "repository": "rhel-7-workstation-rpms",
+        "package": "postgresql-devel-"
+    },
+    {
+        "erratum": "RHSA-2017:3402",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-devel-"
+    },
+    {
+        "erratum": "RHSA-2017:3402",
+        "repository": "rhel-7-workstation-rpms",
+        "package": "postgresql-devel-"
+    }
+]
+
+EXPECTED_POSTGRES_DEVEL_W_REPO = [
+    {
+        "erratum": "RHSA-2017:1983",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-devel-"
+    },
+    {
+        "erratum": "RHSA-2017:2728",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-devel-"
+    },
+    {
+        "erratum": "RHSA-2017:3402",
+        "repository": "rhel-7-server-rpms",
+        "package": "postgresql-devel-"
+    },
+]
+
 PACKAGES = [
-    # package, minimal number of expected errata
-    ('bash-0:4.2.46-20.el7_2.x86_64', 4),
-    ('postgresql-0:9.2.18-1.el7.x86_64', 12),
-    ('postgresql-devel-9.2.18-1.el7.x86_64', 12),
+    # package, expected updates
+    ('bash-0:4.2.46-20.el7_2.x86_64', EXPECTED_BASH),
+    ('postgresql-0:9.2.18-1.el7.x86_64', EXPECTED_POSTGRES),
+    ('postgresql-devel-9.2.18-1.el7.x86_64', EXPECTED_POSTGRES_DEVEL),
 ]
 
 PACKAGES_W_REPOS = [
-    # package, minimal number of expected errata
-    ('bash-0:4.2.46-20.el7_2.x86_64', 2),
-    ('postgresql-0:9.2.18-1.el7.x86_64', 6),
-    ('postgresql-devel-9.2.18-1.el7.x86_64', 6),
+    # package, expected updates
+    ('bash-0:4.2.46-20.el7_2.x86_64', EXPECTED_BASH_W_REPO),
+    ('postgresql-0:9.2.18-1.el7.x86_64', EXPECTED_POSTGRES_W_REPO),
+    ('postgresql-devel-9.2.18-1.el7.x86_64', EXPECTED_POSTGRES_DEVEL_W_REPO),
 ]
 
 REPOS = [
@@ -24,49 +148,34 @@ REPOS = [
 ]
 
 
-EXPECTED_KEYS = [
-    'erratum',
-    'repository',
-    'package',
-]
-
-
-def check_keys(package_records):
-    for record in package_records:
-        for key in EXPECTED_KEYS:
-            assert record[key] is not None, 'Expected key `{}` has no value'.format(key)
-
-
 class TestUpdatesAll(object):
     def test_post_multi(self, rest_api):
         """Tests updates using POST with multiple packages."""
         request_body = tools.updates_body([p[0] for p in PACKAGES])
         updates = rest_api.get_updates(body=request_body).response_check()
         assert len(updates) == len(PACKAGES)
-        for package, min_expected in PACKAGES:
-            assert len(updates[package]) >= min_expected
-            check_keys(updates[package])
+        for package_name, expected_updates in PACKAGES:
+            package = updates[package_name]
+            tools.validate_package_updates(package, expected_updates)
 
-    @pytest.mark.parametrize('package', PACKAGES, ids=[p[0] for p in PACKAGES])
-    def test_post_single(self, rest_api, package):
+    @pytest.mark.parametrize('package_record', PACKAGES, ids=[p[0] for p in PACKAGES])
+    def test_post_single(self, rest_api, package_record):
         """Tests updates using POST with single package."""
-        name, min_expected = package
+        name, expected_updates = package_record
         request_body = tools.updates_body([name])
         updates = rest_api.get_updates(body=request_body).response_check()
         assert len(updates) == 1
-        update, = updates
-        assert len(update) >= min_expected
-        check_keys(update)
+        package, = updates
+        tools.validate_package_updates(package, expected_updates)
 
-    @pytest.mark.parametrize('package', PACKAGES, ids=[p[0] for p in PACKAGES])
-    def test_get(self, rest_api, package):
+    @pytest.mark.parametrize('package_record', PACKAGES, ids=[p[0] for p in PACKAGES])
+    def test_get(self, rest_api, package_record):
         """Tests updates using GET with single package."""
-        name, min_expected = package
+        name, expected_updates = package_record
         updates = rest_api.get_update(name).response_check()
         assert len(updates) == 1
-        update, = updates
-        assert len(update) >= min_expected
-        check_keys(update)
+        package, = updates
+        tools.validate_package_updates(package, expected_updates)
 
 
 class TestUpdatesInRepos(object):
@@ -75,17 +184,17 @@ class TestUpdatesInRepos(object):
         request_body = tools.updates_body([p[0] for p in PACKAGES_W_REPOS], repositories=REPOS)
         updates = rest_api.get_updates(body=request_body).response_check()
         assert len(updates) == len(PACKAGES_W_REPOS)
-        for package, min_expected in PACKAGES_W_REPOS:
-            assert len(updates[package]) >= min_expected
-            check_keys(updates[package])
+        for package_name, expected_updates in PACKAGES_W_REPOS:
+            package = updates[package_name]
+            tools.validate_package_updates(package, expected_updates)
 
-    @pytest.mark.parametrize('package', PACKAGES_W_REPOS, ids=[p[0] for p in PACKAGES_W_REPOS])
-    def test_post_single(self, rest_api, package):
+    @pytest.mark.parametrize(
+        'package_record', PACKAGES_W_REPOS, ids=[p[0] for p in PACKAGES_W_REPOS])
+    def test_post_single(self, rest_api, package_record):
         """Tests updates in repos using POST with single package."""
-        name, min_expected = package
+        name, expected_updates = package_record
         request_body = tools.updates_body([name], repositories=REPOS)
         updates = rest_api.get_updates(body=request_body).response_check()
         assert len(updates) == 1
-        update, = updates
-        assert len(update) >= min_expected
-        check_keys(update)
+        package, = updates
+        tools.validate_package_updates(package, expected_updates)
